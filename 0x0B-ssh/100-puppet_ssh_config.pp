bd3b1c86d4bbe15611b3eb451ed124ad621f8728
Mohamed Super Dumbuya <dumbuya366@gmail.com>
2024-06-16 16:47:37 -0700

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,23 +4,9 @@
 file {'etc/ssh/ssh_config':
 ensure => present,
 content =>"
-	#SSH client configuration
-	host*
-	IdentityFle ~/.ssh/school
+        #SSH client configuration
+        host*
+        IdentityFle ~/.ssh/school
+	PasswordAuthentication no
 	",
-}
-include stdlib
-
-file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-  replace => true,
-}
-
-file_line { 'Delare identity file':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
-  replace => true,
 }
